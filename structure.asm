d13404259cae92cda47d8274ab50aecbf1651ab3
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-06-28 22:28:13 +0200

    Add files via upload

---
+++ structure.asm
@@ -0,0 +1,11 @@
+.model small
+.stack 100h
+.data
+    ; nome_variabile tipo(db o dw) valore
+.code
+.startup
+    ; operazioni con registri e variabili
+
+    mov ah, 4ch ;eseguo il programma
+    int 21h ;termino il programma
+end
