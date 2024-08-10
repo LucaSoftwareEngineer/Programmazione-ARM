faa0b70d6aece15deaa5d643486a54aebe305426
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-08-10 05:35:15 +0200

    Create cerca_elemento_in_array.s

---
+++ cerca_elemento_in_array.s
@@ -0,0 +1,36 @@
+.section .data
+	array: .word 1,8,3
+
+.global start
+start:
+	MOV R0, #8 @valore che cerco nel array
+	LDR R7, =array @indirizzo array
+	
+	@recupero il primo valore
+	LDR R1, [R7,#0]
+	
+	@confronto se è uguale al mio
+	CMP R1, R0
+	BEQ trovato
+	
+	@recupero il secondo valore
+	LDR R1, [R7,#4]
+	
+	@confronto se è uguale al mio
+	CMP R1, R0
+	BEQ trovato
+	
+	@recupero il terzo valore
+	LDR R1, [R7,#4]
+	
+	@confronto se è uguale al mio
+	CMP R1, R0
+	BEQ trovato
+	BNE fine
+	
+trovato:
+	MOV R2, #1
+	B fine
+
+fine:
+	B fine
